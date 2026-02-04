a20d49e73a8594bf2dafd84d4e1ee035c25bcdef
Daniel Karami <45718908+DKMiner@users.noreply.github.com>
2026-02-04 04:19:51 +0400

    Remove phone connection code from conduit

--- conduit/Program.cs
+++ conduit/Program.cs
@@ -7,9 +7,6 @@ namespace Conduit
         public static string APP_NAME = "Mimic Auto Accept";
         public static string VERSION = "2.2.0";
 
-        public static string HUB_WS = "wss://rift.mimic.lol/conduit";
-        public static string HUB = "https://rift.mimic.lol";
-
         private static App _instance;
 
         [STAThread]
